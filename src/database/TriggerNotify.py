8b1011fa0bdd96752af22b195a99b80f91c8541f
localhostPIB <obosslet@htwsaar.de>
2021-09-20 12:02:07 +0200

    Create TriggerNotify.py

---
+++ src/database/TriggerNotify.py
@@ -0,0 +1,28 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+import select
+
+
+def dblisten():
+    connection = psycopg2.connect(
+            host="128.0.0.0",
+            database="BD",
+            user="###",
+            password="....")
+
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = connection.cursor()
+    cur.execute("LISTEN new_Id;")
+    print("Starte")
+    while True:
+        select.select([connection], [], [])
+        connection.poll()
+        connection.commit()
+
+        while connection.notifies:
+            notify = connection.notifies.pop()
+            print("Got NOTIFY:", notify.pid, notify.channel, notify.payload)
+
+
+if __name__ == '__main__':
+    dblisten()
