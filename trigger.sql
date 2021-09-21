b432d2f7446c8fbc66c8e94b048e223d71d3863a
localhostPIB <obosslet@htwsaar.de>
2021-09-21 22:45:54 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,21 +1,36 @@
 create trigger trigger1 
 after insert or update on public."FileInfos" 
-for each row execute procedure notify_id_trigger();
+for each row 
+execute procedure notify_id_trigger();
 
 
 create or replace function notify_id_trigger() 
 RETURNS trigger AS $$
 begin perform pg_notify('new_id'::text, new::text); 
-return new; end; $$ language plpgsql;
+return new; 
+end; 
+$$ language plpgsql;
 
 
 create or replace function notify_id_trigger() 
 RETURNS trigger AS $$
+begin 
+perform pg_notify('new_id'::text, new."LastModifiedDateTime"::text || new."FileName"::text); 
+return new; 
+end; 
+$$ language plpgsql;
+
+create or replace function notify_id_trigger() 
+RETURNS trigger AS $$
 begin perform pg_notify('new_id'::text, new."Id"::text); 
-return new; end; $$ language plpgsql;
+return new; 
+end; 
+$$ language plpgsql;
 
 
 create or replace function notify_id_trigger() 
 RETURNS trigger AS $$
 begin perform pg_notify('new_id'::text, new."LastModifiedDateTime"::text); 
-return new; end; $$ language plpgsql;
+return new; 
+end; 
+$$ language plpgsql;
