9cffbb8ff8f646fc1f8abcb546dc8988aab447fb
localhostPIB <obosslet@htwsaar.de>
2021-10-21 16:56:07 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,35 +1,51 @@
 ---------------------------- Delete Trigger ------------------------------------
 
-create trigger trigger2 
-after DELETE on public."FileInfos" 
-for each row 
-execute procedure notify_del_trigger();
-
-create or replace function notify_del_trigger() 
+create or replace function delete_id_trigger() 
 RETURNS trigger AS $$
 begin perform pg_notify('del_id'::text, old::text); 
 return new; 
 end; 
 $$ language plpgsql;
 
+
+create trigger triggerDelete 
+after delete on public."FileInfos" 
+for each row 
+execute procedure delete_id_trigger();
+
 --------------------------- Delete Command -------------------------------------
 DELETE FROM public."FileInfos" WHERE "FileName" = 'TestFile'
 
 ------------------------------------- Create Trigger ---------------------------
-create trigger trigger1 
-after insert or update on public."FileInfos" 
-for each row 
-execute procedure notify_id_trigger();
-
-
-create or replace function notify_id_trigger() 
+create or replace function create_id_trigger() 
 RETURNS trigger AS $$
-begin perform pg_notify('new_id'::text, new::text);  -- wichtig !!
+begin perform pg_notify('new_id'::text, new::text); 
 return new; 
 end; 
 $$ language plpgsql;
 
 
+create trigger triggerCreate 
+after insert on public."FileInfos" 
+for each row 
+execute procedure create_id_trigger();
+
+------------------------------------- Update Trigger ---------------------------
+
+create or replace function update_id_trigger() 
+RETURNS trigger AS $$
+begin perform pg_notify('update_id'::text, new::text); 
+return new; 
+end; 
+$$ language plpgsql;
+
+
+create trigger triggerUpdate
+after update on public."FileInfos" 
+for each row 
+execute procedure update_id_trigger();
+
+---------------------------------------------------------------------------------
 create or replace function notify_id_trigger() 
 RETURNS trigger AS $$
 begin 
