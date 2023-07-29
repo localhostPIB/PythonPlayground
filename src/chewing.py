6a200359ec3cabf654fa5ccebd5532d60db7642f
localhostPIB <nurschule@gmail.com>
2023-07-29 19:49:29 +0200

    Add files via upload

---
+++ src/chewing.py
@@ -0,0 +1,32 @@
+try:
+    import schedule
+except ImportError:
+    from pip._internal import main as pip
+    pip(['install', 'schedule'])
+    import schedule
+
+
+import time
+
+try:
+    from playsoundx import playsoundx
+except ImportError:
+    from pip._internal import main as pip
+    pip(['install', 'playsound==1.2.2'])
+    from playsound import playsound
+
+
+def play_sound():
+    playsound('https://cdn.freesound.org/previews/412/412068_5121236-lq.mp3')
+
+
+def start():
+    schedule.every(3).minutes.do(play_sound)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    start()
